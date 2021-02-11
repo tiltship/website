a1208a03150f9a7f23d25c75ee5f8f8880ff0d69
Nandan Rao <nandanmarkrao@gmail.com>
2021-02-11 11:00:06 +0100

    added event category to gtag dwelled event

--- client/src/events.js
+++ client/src/events.js
@@ -1,6 +1,8 @@
 export function spent(seconds, tag) {
   setTimeout(() => {
-    gtag('event', tag, {})
+    gtag('event', tag, {
+      event_category: 'engagement'
+    })
     fbq('trackCustom', tag)
   }, seconds * 1000)
 }
