5c8ee4c915fc7cf9b9d5187aabc10768dcc7dc9d
Nandan Rao <nandanmarkrao@gmail.com>
2021-02-15 20:32:27 +0100

    added linting

---
+++ client/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "rules": {
+  },
+  plugins: [
+    'svelte3'
+  ],
+  overrides: [
+    {
+      files: ['*.svelte'],
+      processor: 'svelte3/svelte3'
+    }
+  ],
+  "globals": {
+    "process": true
+  }
+
+};
